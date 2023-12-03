7ca7450e2bab35e9b73910c47efc1b89ee6e8d02
@Marco2020333 <2020333@student.cct.ie>
2023-12-03 20:32:30 +0000

    Commit question c
    Apply Min-Max Normalization, Z-score Standardization and Robust scalar

--- CA1_DEP/CA1-DEP.R
+++ CA1_DEP/CA1-DEP.R
@@ -6,12 +6,16 @@ library(tidyverse)
 
 install.packages(c("tidyverse","dummy","caTools","conflicted"))
 install.packages("skimr")
+install.packages("caret")
+
 
 library(conflicted)
 library(tidyverse)
 library(dummy)
 library(caTools)
 library(skimr)
+library(dplyr)
+library(caret)
 
 covid_2022 <-read_csv(file="covid_2022.csv")
 view(covid_2022)
@@ -30,5 +34,13 @@ glimpse(covid_2022)
 #
 summary(covid_2022)
 
+#
+preprocess_num <-preProcess(covid_2022[, c(2, 3,4,5,6,10)], method=c('center','scale'))
+
+#
+data_Process <-predict(preprocess_num,covid_2022[, c(2, 3,4,5,6,10)])
+
+#
+summary(data_Process)
 
 
